b66739769385d9c85d7a303fab8e3e31a9ecd998
dzak2323 <23dzakie@gmail.com>
2026-09-04 06:15:59 +0800

    penyesuaian format gambar dan peletakan tulisan

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -6,61 +6,75 @@ void main() => runApp(const MyApp());
 // CLASS 1: MyApp
 // ==========================================
 class MyApp extends StatelessWidget {
-  const MyApp({super.key}); // <-- Jangan ada kurung kurawal tutup } di baris ini!
+  const MyApp({
+    super.key,
+  }); // <-- Jangan ada kurung kurawal tutup } di baris ini!
 
   @override
   Widget build(BuildContext context) {
     const String appTitle = 'Flutter layout demo';
-    
+
     return MaterialApp(
       title: appTitle,
       theme: ThemeData(
         textTheme: const TextTheme(
-          bodyMedium: TextStyle(
-            color: Colors.white,
-            fontSize: 16,
-          ),
+          bodyMedium: TextStyle(color: Colors.white, fontSize: 16),
         ),
       ),
-      home: Scaffold( // <-- Buka Scaffold
+      home: Scaffold(
+        // <-- Buka Scaffold
         backgroundColor: const Color.fromARGB(255, 7, 26, 55),
 
         // appBar: AppBar(title: const Text(appTitle)),
-        body: const SingleChildScrollView( // <-- Buka SingleChildScrollView  
-          child: Column( // <-- Buka Column
-            children: [
-
-              ImageSection(
-                image: 'images/Foto_saya.jpg',
-              ),
-
-              // Memanggil class TitleSection
-              TitleSection(
-                name: 'Muhammad Dzaki RifaI',
-                NIM: '2409106056',
-                location: 'Kelas B24, Informatika, Universitas Mulawarman',
-              ),
-              // ButtonSection(),
-              TextSection(
-                description:
-'''
+        body: const SingleChildScrollView(
+          // <-- Buka SingleChildScrollView
+          child: Padding(
+            padding: EdgeInsets.all(60.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start, // <-- Buka Column
+              children: [
+                ImageSection(image: 'images/Foto_saya.jpg'),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    // Memanggil class TitleSection
+                    children: [
+                      TitleSection(
+                        name: 'Muhammad Dzaki RifaI',
+                        NIM: '2409106056',
+                        location:
+                            'Kelas B24, Informatika, Universitas Mulawarman',
+                      ),
+                      // ButtonSection(),
+                      TextSection(
+                        description: '''
 Perkenalkan saya Muhammad Dzaki RifaI, seorang mahasiswa di Kelas B24, Program Studi Informatika, Universitas Mulawarman
 saat ini saya diminta membuat prototype aplikasi Flutter dengan tema "website pribadi" yah fungsinya porto lah ya
 buat ini sambil baca Documentation + minta bantuan gemini terjemahkan karena bahasa dari documentation nya teknis banget
-okey sekian dari saya terimakasih''',)
-            ],
-          ), // <-- Tutup Column
-        ), // <-- Tutup SingleChildScrollView
-      ), // <-- Tutup Scaffold (Tadi ini yang hilang!)
-    ); // <-- Tutup MaterialApp
+okey sekian dari saya terimakasih''',
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 } // <-- MyApp BARU DITUTUP DI SINI!
 
 // ==========================================
-// CLASS 2: TitleSection 
+// CLASS 2: TitleSection
 // ==========================================
 class TitleSection extends StatelessWidget {
-  const TitleSection({super.key, required this.name, required this.location, required this.NIM});
+  const TitleSection({
+    super.key,
+    required this.name,
+    required this.location,
+    required this.NIM,
+  });
 
   final String name;
   final String location;
@@ -78,7 +92,9 @@ class TitleSection extends StatelessWidget {
               children: [
                 Padding(
                   padding: const EdgeInsets.only(bottom: 8),
-                  child: Text(name,style: const TextStyle(fontWeight: FontWeight.bold),
+                  child: Text(
+                    name,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
                   ),
                 ),
                 Text(NIM, style: TextStyle(color: Colors.grey[500])),
@@ -123,12 +139,10 @@ class TextSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(32),
-      child: Text(description, softWrap: true, textAlign: TextAlign.left,),
-      
+      child: Text(description, softWrap: true, textAlign: TextAlign.left),
     );
   }
 }
-
 
 class ButtonWithText extends StatelessWidget {
   const ButtonWithText({
@@ -171,11 +185,12 @@ class ImageSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset(
-      image,
-      width: 200, // Lebar gambar
-      height: 200, // Tinggi gambar
-      fit: BoxFit.cover, // Memaksa gambar mengisi penuh area tanpa merubah proporsi aslinya
+    return Padding(
+      padding: const EdgeInsets.only(right: 40.0, top: 20.0),
+      child: CircleAvatar(
+        radius: 100,
+        backgroundImage: AssetImage(image),
+      ), // Memaksa gambar mengisi penuh area tanpa merubah proporsi aslinya
     );
   }
 }
